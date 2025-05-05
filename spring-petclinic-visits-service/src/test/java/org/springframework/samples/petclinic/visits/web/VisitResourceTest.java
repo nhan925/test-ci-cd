fae61eb909bf5d37fafba7f0b5c91f4cbbc363bc
Nguyen Trong Nhan <info.nhan925@gmail.com>
2025-05-06 02:50:49 +0700

    test

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
@@ -22,7 +22,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(VisitResource.class)
 @ActiveProfiles("test")
 class VisitResourceTest {
-    
     @Autowired
     MockMvc mvc;
 
